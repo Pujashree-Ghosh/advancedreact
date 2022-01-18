0386fe45efbcdef65c501375ab7277348b469d83
Pujashree-Ghosh <you@example.com>
2022-01-19 01:47:15 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Fetch from "./components/apifetch";
+import Post from "./components/apipost";
+// import Form from "./components/form";
+// import Postform from "./components/postform";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Fetch /> */}
+        <Post />
+        {/* <Form /> */}
+        {/* <Postform /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
