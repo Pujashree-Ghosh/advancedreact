73ce587041d11a2482e00df9c6fb6a74160f2d13
Pujashree-Ghosh <you@example.com>
2022-01-27 11:55:14 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-import React, { Component } from "react";
+import React from "react";
+
+// import React, { useReducer } from "react";
 
 //API Handling
 
@@ -37,49 +39,107 @@ import React, { Component } from "react";
 // export const ChannelContext = React.createContext();
 
 //ReducerHook
-import Reducer from "./components/hooks/useReducer/ReducerCounter";
+// import Reducer from "./components/hooks/useReducer/ReducerCounter";
+// import ObjectReducer from "./components/hooks/useReducer/ObjectStateAction";
+// import MultipleReducer from "./components/hooks/useReducer/MultipleUseReducer";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <Fetch /> */}
-        {/* <Post /> */}
-        {/* <Form /> */}
-        {/* <Postform /> */}
-        {/* ---------------Hooks-------- */}
-        {/* ----------useState---------- */}
-        {/* <ClassCounter /> */}
-        {/* <HookCounter /> */}
-        {/* <PrevStateHook /> */}
-        {/* <ObjectVar /> */}
-        {/* <ArrayVar /> */}
-        {/* ----------useEffect------- */}
-        {/* <ClassCounter /> */}
-        {/* <Effect /> */}
-        {/* <ConditionalEffect /> */}
-        {/* <EffectOnce /> */}
-        {/* <CleanUp /> */}
-        {/* <IntervalCounter /> */}
-        {/* <IntervalHook /> */}
-        {/* <Fetch /> */}
-        {/* <FetchbyId /> */}
-        {/* <TriggerEffect /> */}
+//useReducer with useContext
+// import ContextReducer1 from "./components/hooks/useReducer/ContextReducer1";
+// import ContextReducer2 from "./components/hooks/useReducer/ContextReducer2";
+// import ContextReducer3 from "./components/hooks/useReducer/ContextReducer3";
 
-        {/* -----------useContext----------- */}
+//step1-creating counter using reducer
 
-        {/* <UserContext.Provider value={"Pujashree"}> */}
-        {/* <ChannelContext.Provider value={"Bit Canny"}> */}
-        {/* <ContextA /> */}
-        {/* <MainContext /> */}
-        {/* </ChannelContext.Provider> */}
-        {/* </UserContext.Provider> */}
+// const initialState = 0;
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case "increment":
+//       return state + 1;
+//     case "decrement":
+//       return state - 1;
+//     case "reset":
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
 
-        {/* ---------reducer hook-------- */}
-        <Reducer />
-      </div>
-    );
-  }
+//step2-provide and consume the counter context in the required components
+
+// export const CountContext = React.createContext();
+
+//---------------useRef Hook---------
+
+// import Focus from "./components/hooks/useRef/FocusInput";
+// import ClassTimer from "./components/hooks/useRef/ClassTimer";
+// import HookTimer from "./components/hooks/useRef/HookTimer";
+
+//UseNavigate
+
+import Navigate from "./components/hooks/useNavigate/Example1";
+
+function App() {
+  // const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="App">
+      {/* <Fetch /> */}
+      {/* <Post /> */}
+      {/* <Form /> */}
+      {/* <Postform /> */}
+      {/* ---------------Hooks-------- */}
+      {/* ----------useState---------- */}
+      {/* <ClassCounter /> */}
+      {/* <HookCounter /> */}
+      {/* <PrevStateHook /> */}
+      {/* <ObjectVar /> */}
+      {/* <ArrayVar /> */}
+      {/* ----------useEffect------- */}
+      {/* <ClassCounter /> */}
+      {/* <Effect /> */}
+      {/* <ConditionalEffect /> */}
+      {/* <EffectOnce /> */}
+      {/* <CleanUp /> */}
+      {/* <IntervalCounter /> */}
+      {/* <IntervalHook /> */}
+      {/* <Fetch /> */}
+      {/* <FetchbyId /> */}
+      {/* <TriggerEffect /> */}
+      {/* -----------useContext----------- */}
+      {/* <UserContext.Provider value={"Pujashree"}> */}
+      {/* <ChannelContext.Provider value={"Bit Canny"}> */}
+      {/* <ContextA /> */}
+      {/* <MainContext /> */}
+      {/* </ChannelContext.Provider> */}
+      {/* </UserContext.Provider> */}
+      {/* ---------reducer hook-------- */}
+      {/* <Reducer /> */}
+      {/* <ObjectReducer /> */}
+      {/* <MultipleReducer /> */}
+
+      {/* ----------Reducer with Context--------- */}
+      {/* <div>
+        <CountContext.Provider
+          value={{ countState: count, countDispatch: dispatch }}
+        >
+          <div>
+            Count- {count}
+            <ContextReducer1 />
+            <ContextReducer2 />
+            <ContextReducer3 />
+          </div>
+        </CountContext.Provider>
+      </div> */}
+
+      {/* -----------Ref Hook---------- */}
+
+      {/* <Focus /> */}
+      {/* <ClassTimer /> */}
+      {/* <HookTimer /> */}
+
+      {/* -------Navigate---------- */}
+      <Navigate />
+    </div>
+  );
 }
 
 export default App;
